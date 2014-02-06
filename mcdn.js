7266e86c5ae4477a820cfa855aeefd407b81515b
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-02-06 19:21:37 +0100

    Adding actual IDL modal

--- mcdn.js
+++ mcdn.js
@@ -1,8 +1,18 @@
 var tdwfb_config = {
 	disableDate: true
 };
-(function(){
+window._idl = {};
+_idl.variant = "modal";
+(function() {
+	var idl = document.createElement('script');
+	idl.type = 'text/javascript';
+	idl.async = true;
+	idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || 
+'') + '&variant=' + (_idl.variant || 'banner');
+	document.getElementsByTagName('body')[0].appendChild(idl);
+
 	var e = document.createElement('script'); e.type='text/javascript'; e.async = true;
 	e.src = document.location.protocol + '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s);
 })();
+
