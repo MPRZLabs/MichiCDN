b5b8469c6ebdf6bdec5702e259cc460462f4eef2
Michał Sidor (Michcioperz) <michcioperz@autistici.org>
2014-02-06 19:13:03 +0100

    Adding The Day We Fight Back

---
+++ README.md
@@ -0,0 +1,1 @@
+This code is appended to every website run by Michcioperz. Why we do it is to have an easier way to add stuff like IDL banners to the whole server at once.
---
+++ mcdn.js
@@ -0,0 +1,8 @@
+var tdwfb_config = {
+	disableDate: true
+};
+(function(){
+	var e = document.createElement('script'); e.type='text/javascript'; e.async = true;
+	e.src = document.location.protocol + '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(e, s);
+})();
